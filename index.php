5c132b9f4a1e7e98f974428ecf6b9ed2fcf50644
save <nick.c@menisch.ch>
2022-08-29 20:53:56 +0200

    skillbar + php added

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
                 <li><a href="#home" class="nav-link">Home</a> </li>
                 <li><a href="#about" class="nav-link">About</a> </li>
                 <li><a href="#projects" class="nav-link">Projects</a> </li>
+                <li><a href="#skills" class="nav-link">Skills</a> </li>
                 <li><a href="#contact" class="nav-link">Contact</a> </li>
             </ul>
             <div class="menu-icon">
@@ -47,7 +48,8 @@
             </div>
             <div class="about-text">
                 <p>Hello. My Name is Nick.</p>
-                <p> I am from Switzerland and making an apprenticeship as computer scientist.</p>
+                <p>I'm a Swiss Student, making an apprenticeship as a computer scientist in Zürich.</p>
+                <p>My main program languages are Java and JavaScript, but im open to learn new languages.</p>
             </div>
         </div>
     </section>
@@ -132,15 +134,113 @@
             </div>
         </div>
     </section>
+    <section class="skills container" id="skills">
+        <h2 class="heading">Skills</h2>
+        <div class="skills-content">
+            <div class="skill">
+                <div class="skill-name">Java</div>
+                <div class="skill-bar">
+                    <div class="skill-per" per="90%" style="max-width: 90%;"></div>
+                </div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">JavaScript</div>
+                <div class="skill-bar">
+                    <div class="skill-per" per="80%" style="max-width: 80%;"></div>
+                </div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">Python</div>
+                <div class="skill-bar">
+                    <div class="skill-per" per="70%" style="max-width: 70%;"></div>
+                </div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">HTML</div>
+                <div class="skill-bar">
+                    <div class="skill-per" per="75%" style="max-width: 75%;"></div>
+                </div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">CSS</div>
+                <div class="skill-bar">
+                    <div class="skill-per" per="60%" style="max-width: 60%;"></div>
+                </div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">MySQL</div>
+                <div class="skill-bar">
+                    <div class="skill-per" per="65%" style="max-width: 65%;"></div>
+                </div>
+            </div>
+        </div>
+    </section>
     <section class="contact container" id="contact">
+        <?php
+            //Initialize Variables
+            $nameErr = $emailErr = $messageErr = "";
+            $name = $email = $message = "";
+
+            //Wenn senden gedrückt wurde
+            if (isset($_POST['submit'])) {
+                //Name überprüfung
+                if (empty($_POST["name"])) {
+                    $nameErr = "Bitte geben sie einen Name ein.";
+                } else {
+                    $name = test_input($_POST["name"]);
+                    //check name on RegEx
+                    if (!preg_match("/^[a-zA-Z-' ]{2,}$/", $name)) {
+                        $nameErr = "Es sind nur Buchstaben erlaubt.";
+                    }
+                }
+                //check message
+                if (empty($_POST["message"])) {
+                    $messageErr = "Bitte geben sie eine Nachricht ein.";
+                } else {
+                    $message = test_input($_POST["message"]);
+                }
+                //echek email
+                if (empty($_POST["email"])) {
+                    $emailErr = "Bitte geben sie eine verfügbare Email ein.";
+                } else {
+                    $email = test_input($_POST["email"]);
+                    //check email on RegEx
+                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $emailErr = "Geben sie eine formal gerechte email ein.";
+                    }
+                }
+
+            }
+            //tests input on cross-scripting
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+            return $data;
+            }
+        ?>
         <h2 class="heading">Contact</h2>
         <form action="" class="contact-form" id="contact-form">
-            <input type="text" placeholder="Your Name" class="name" required>
-            <input type="email" name="" id="" placeholder="example@example.ch" class="email" required>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Text her ...." class="message" required></textarea>
+            <span>Contact Me</span>
+            <input type="text" placeholder="Your Name" class="name" id="name" required>
+            <input type="email" name="email" id="email" placeholder="example@example.ch" class="email" required>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Text here ...." class="message" required></textarea>
             <input type="submit" value="Send" class="send-btn">
         </form>
+        <?php
+        if ($nameErr === ''  && $emailErr === '' && $messageErr === '' && isset($_POST['submit']) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+            //mail('mail', 'Message from Portfolio', $message, "From: $email");
+            $name = "";
+            $message= "";
+            $email = "";
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+            echo "hello";
+        }
+        ?>
     </section>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
